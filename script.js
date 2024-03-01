ed3cbe5704d870066245b788cb399e79b8123030
Jim Lin-Dybner <j.lin130@gmail.com>
2024-03-01 17:07:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,60 @@
-// VARIABLES
-const numOneInput = document.querySelector("#num-1");
-const numTwoInput = document.querySelector("#num-2");
-const btnMultiply = document.querySelector("#btn-multiply");
-const btnDivide = document.querySelector("#btn-divide");
-const resultOutput = document.querySelector("#result-output");
+window.onload = () => {
+    // VARIABLES
+    const numOneInput = document.querySelector("#num-1");
+    const numTwoInput = document.querySelector("#num-2");
+    const btnMultiply = document.querySelector("#btn-multiply");
+    const btnDivide = document.querySelector("#btn-divide");
+    const resultOutput = document.querySelector("#result-output");
 
-// FUNCTIONS
-// multiply
-function multiply(numOne, numTwo) {
-    result = numOne * numTwo;
-    return result;
-}
-
-// divide
-function divide(numOne, numTwo) {
-    result = numOne / numTwo;
-    return result;
-}
-
-// process multiply click
-function processMultiply() {
-    // prevent form submission
-    event.preventDefault();
-
-    // collect inputs
-    let numOne = parseFloat(numOneInput.value);
-    let numTwo = parseFloat(numTwoInput.value);
-    
+    // FUNCTIONS
     // multiply
-    let result = multiply(numOne, numTwo);
-    
-    // output result
-    resultOutput.innerHTML = result;
-}
-
-// process divide click
-function processDivide() {
-    // prevent form submission
-    event.preventDefault();
-
-    // collect inputs
-    let numOne = parseFloat(numOneInput.value);
-    let numTwo = parseFloat(numTwoInput.value);
+    const multiply = (numOne, numTwo) => {
+        result = numOne * numTwo;
+        return result;
+    }
 
     // divide
-    let result = divide(numOne, numTwo);
+    const divide = (numOne, numTwo) => {
+        result = numOne / numTwo;
+        return result;
+    }
 
-    // output result
-    resultOutput.innerHTML = result;
+    // process multiply click
+    const processMultiply = event => {
+        // prevent form submission
+        event.preventDefault();
+
+        // collect inputs
+        let numOne = parseFloat(numOneInput.value);
+        let numTwo = parseFloat(numTwoInput.value);
+        
+        // multiply
+        let result = multiply(numOne, numTwo);
+        
+        // output result
+        resultOutput.innerHTML = result;
+    }
+
+    // process divide click
+    const processDivide = event => {
+        // prevent form submission
+        event.preventDefault();
+
+        // collect inputs
+        let numOne = parseFloat(numOneInput.value);
+        let numTwo = parseFloat(numTwoInput.value);
+
+        // divide
+        let result = divide(numOne, numTwo);
+
+        // output result
+        resultOutput.innerHTML = result;
+    }
+
+    // EVENT LISTENERS
+    // multiply
+    btnMultiply.addEventListener("click", processMultiply);
+
+    // divide
+    btnDivide.addEventListener("click", processDivide);
 }
-
-// EVENT LISTENERS
-// multiply
-btnMultiply.addEventListener("click", processMultiply);
-
-// divide
-btnDivide.addEventListener("click", processDivide);
